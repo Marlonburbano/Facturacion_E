88d4fe0ca1fa26650fa5af7d171dc5afaf834515
Marlon Burbano <marlonburbano2899@gmail.com>
2023-07-19 15:45:55 -0500

    Facturacion E 2

---
+++ APISistemaVentas/SistemaVenta.DAL/Repositorios/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaVenta.DAL.Repositorios.Contrato;
+using SistemaVenta.DAL.DBCOntext;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+public class GenericRepository<TModelo>:IGenericRepository<TModelo> where TModelo : class
+{
+    private readonly DbventaContext dbcontext;
+
+    public GenericRepository(DbventaContext dbcontext)
+    {
+        _dbcontext = dbcontext;
+    }
+
+    public Task<IQueryable<TModelo>> Consultar(Expression<Func<TModelo, bool>>? filtro = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TModelo> Crear(TModelo modelo)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Editar(TModelo modelo)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Eliminar(TModelo modelo)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TModelo> Obtener(Expression<Func<TModelo, bool>> filtro)
+    {
+        throw new NotImplementedException();
+    }
+
+}
+ 
