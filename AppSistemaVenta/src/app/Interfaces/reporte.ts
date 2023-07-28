c6823e13cff8d0050e94bd187583098e3cdce49c
Marlon Burbano <marlonburbano2899@gmail.com>
2023-07-28 13:04:35 -0500

    Facturacion E P7

---
+++ AppSistemaVenta/src/app/Interfaces/reporte.ts
@@ -0,0 +1,10 @@
+export interface Reporte {
+ numeroDocumento:string,
+ tipoPago:string,
+ fechaRegistro:string,
+ totalVenta:string,
+ Producto:string,
+ cantidad:string,
+ precio:string,
+ total:string   
+}
